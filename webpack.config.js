9bddc70ab61af55e10209a5ebe50fdb46710991f
Vincent Song <songvc@users.noreply.github.com>
2016-05-25 15:44:51 -0700

    building form

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      exclude: /node_modules/,
+      loader: 'babel',
+      query:
+      {
+          presets:['es2015', 'react'],
+          plugins: ["transform-object-rest-spread"]
+      }
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './'
+  }
+};
